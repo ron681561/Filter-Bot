ee562c52c3424f8b1206ec5f31cfd1f6be114f7a
ron681561 <68684946+ron681561@users.noreply.github.com>
2020-07-23 14:04:06 +0530

    __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -22,7 +22,7 @@ PM_START_TEXT = """
 
 ```You You Can Add any kind of Filters to This Bot!```
 
-*Channel:© @DX_Botz*
+Thanks To *DarlAngel* And [Unknown Hacker X](https://t.me/Unknown_Hacker_X) . 
 
 /help for more details..
 """
@@ -38,7 +38,7 @@ And the following:
 """.format(dispatcher.bot.first_name, "" if not ALLOW_EXCL else "\nഈ പറഞ്ഞിരിക്കുന്ന commandകൾ എല്ലാം  / അല്ലെങ്കിൽ ! വെച്ച് ഉപയോഗിക്കാവുന്നതാണ്...\n")
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
-It took lots of work for [my creator](t.me/sonoflars) to get me to where I am now, and every donation helps \
+It took lots of work for [my creator](t.me/unknown_hacker_x) to get me to where I am now, and every donation helps \
 motivate him to make me even better. All the donation money will go to a better VPS to host me, and/or beer \
 (see his bio!). He's just a poor student, so every little helps!
 There are two ways of paying him; [PayPal](paypal.me/PaulSonOfLars), or [Monzo](monzo.me/paulnionvestergaardlarsen)."""
